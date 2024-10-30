3f87fed4f9647955e540c2449a4b7bd041c5500b
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-10-30 13:57:54 -0700

    Add files via upload
    Master Form Validation: Dropdown Menu Validation in JavaScript!

---
+++ script.js
@@ -0,0 +1,21 @@
+// chapter 83 - Form validation : drop-downs 
+
+// validation function 
+// function checkForSelection(selectId) {
+//     var target = document.getElementById('states');
+//     if (target.selectedIndex === 0) {
+//       alert("please select a state");
+//       return false; // prevents from submission
+//     }
+// }
+
+
+// improving usability
+// function checkForSelection(selectId) {
+//     var target = document.getElementById('states');
+//     if (target.selectedIndex === 0) {
+//       alert("please select a state");
+//       target.focus();// Move focus back to the drop-down
+//       return false; // prevents from submission
+//     }
+// }
